e60e6bb9d7602f13e44ac16358d9454a7ff855a4
dduartevla <deboraduarte@ice.ufjf.br>
2022-05-25 14:57:10 -0300

    Criação dos métodos para adicionar nomes a lista. Criação do método para sortear um nome

--- app/src/main/java/ufjf/dcc/debora/sorteianome/MainActivity.java
+++ app/src/main/java/ufjf/dcc/debora/sorteianome/MainActivity.java
@@ -3,6 +3,11 @@ package ufjf.dcc.debora.sorteianome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    ArrayList<String> nomes = new ArrayList<>();
+
+    public void adicionaNome(View origem){
+
+        //Captura o nome digitado
+        EditText editTextNome = findViewById(R.id.editTextNome);
+
+        //Trabalha o nome capturado
+        String nome = editTextNome.getText().toString();
+
+        //Adiciona na lista
+        nomes.add(nome);
+
+        //Limpa campo de nome
+        editTextNome.setText("Insira novo nome");
+
+        //Atualiza contagem de nomes
+        int cont = nomes.size();
+        TextView editTextCont = findViewById(R.id.textViewQtdAdicionados);
+        editTextCont.setText(cont+"");
+
+    }
+
+    public void sorteiaNome(View origem){
+
+        //Fazendo o sorteio
+        int min = 0;
+        int max = nomes.size();
+
+        int posicao = (int)Math.floor(Math.random()*(max-min+1)+min);
+
+        //Informando nome sorteado na tela
+
+        TextView nomeSorteado = findViewById(R.id.textViewNomeSortado);
+        nomeSorteado.setText(nomes.get(posicao));
     }
 }
